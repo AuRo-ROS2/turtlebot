6813a811687dd432b1e4cce1711eca9c51ba26a1
Adam Jacobs <adamjacobs272@gmail.com>
2024-04-07 21:43:15 -0400

    cam launch

--- launch/launch.py
+++ launch/launch.py
@@ -1,19 +1,56 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node, ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='usb_cam',
-            executable='usb_cam_node_exe',
-            name='usb_cam_node',
-            parameters=[{'video_device': '/dev/video11'}] 
-        ),
-       
-        Node(
-            package='rqt_image_view',
-            executable='rqt_image_view',
-            name='rqt_image_view',
-            
-        )
-    ])
+
+    depthai_prefix = get_package_share_directory("depthai_ros_driver")
+    rviz_config = os.path.join(depthai_prefix, "config", "rgbd.rviz")
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument("use_rviz", default_value="False"),
+            Node(
+                condition=IfCondition(LaunchConfiguration("use_rviz")),
+                package="rviz2",
+                executable="rviz2",
+                name="rviz2",
+                output="log",
+                arguments=["-d", rviz_config],
+            ),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    os.path.join(depthai_prefix, "launch", "description.launch.py")
+                )
+            ),
+            ComposableNodeContainer(
+                name="container",
+                namespace="",
+                package="rclcpp_components",
+                executable="component_container",
+                composable_node_descriptions=[
+                    # Driver itself
+                    ComposableNode(
+                        package="depthai_ros_driver",
+                        plugin="depthai_ros_driver::RGBDCamera",
+                        name="camera",
+                        parameters=[
+                            {
+                                "i_rgb_fps": 60.0,
+                                "i_enable_depth": False,
+                                "i_enable_lr": False,
+                            }
+                        ],
+                    ),
+                ],
+                output="screen",
+            ),
+        ]
+    )
