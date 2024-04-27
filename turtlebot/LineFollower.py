abddd3429040278795e83f0b9c4b6705ff06e858
Adam Jacobs <adamjacobs272@gmail.com>
2024-04-26 21:47:31 -0400

    straight line following

--- turtlebot/LineFollower.py
+++ turtlebot/LineFollower.py
@@ -11,92 +11,112 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
 
+
 LINEAR_VEL = 1.5
 ANGULAR_VEL = 1.5
-RGB_LOW = (150,90,0)
-RGB_HIGH = (255,180,100)
+RGB_LOW = (0,0,0)
+RGB_HIGH = (255,180,150)
 
 FPS = 15.0
 DISPLAY_FPS = 5.0
 DISPLAY_IMG = True
-IMG_W = 640
-IMG_H = 480
+IMG_W = 250
+IMG_H = 250
 K = ANGULAR_VEL * 2
 
 
 
 class WebcamControl():
     def __init__(self):
-        print("hello")
-        rclpy.init()
         self.mutex = Lock()
+        rclpy.init()
+
+        
         self.node = rclpy.create_node('line_follower_node')
-        print("hello1")
-        self.img_pub = self.node.create_publisher(Image, 'usb_cam/image_raw', qos_profile=qos_profile_sensor_data)
+        #recieves image from camera
+        self.img_sub = self.node.create_subscription(Image, '/oakd/rgb/preview/image_raw', self.process_image, 5)
+        self.img_line = self.node.create_publisher(Image, '/imgage/line', 3)
+        
+        #publishes to create3 to operate robot
         self.cmd_vel_pub = self.node.create_publisher(Twist, 'cmd_vel', qos_profile=qos_profile_sensor_data)
-        print("hello2")
+        
         self.bridge = CvBridge()
-
-        self.cam = cv2.VideoCapture(0)
-
-        #self.timer = self.node.create_timer(1.0 / FPS, self.process_image)
+        
         try:
             print("Node is running")
             rclpy.spin(self.node)
+            
         except KeyboardInterrupt:
             print("Rospy Spin Shut down")
 
-    def process_image(self):
-        ret_val, img = self.cam.read()
+    def process_image(self, input_image):
+        print("recieved image")
 
-        if ret_val:
-            cx, img = self.get_line_pos(img)
+        #command = Twist()
+        #command.linear.x = 0.5
+        #self.cmdel_pub.publish(command)
+        
 
+        try:
+            img = self.bridge.imgmsg_to_cv2(input_image, "bgr8")
+            print(img.shape)
+        except CvBridgeError as e:
+            print(e)
+
+
+        if img is None:
+            print ('frame dropped, skipping tracking')
+        else:
+            cx = self.get_line_pos(img)
+            print(cx)
             if cx is not None:
                 error = IMG_W/2 - cx
                 error_norm = error / (IMG_W / 2.0)
-
+                
                 command = Twist()
                 command.linear.x = LINEAR_VEL * (1 - np.abs(error_norm))
                 command.angular.z = K * error_norm
+                self.cmd_vel_pub.publish(command)
             else:
                 command = Twist()
-                command.linear.x = 0
+                command.linear.x = 0.0
                 command.angular.z = ANGULAR_VEL
-
-            self.cmd_vel_pub.publish(command)
-
-            if DISPLAY_IMG:
-                try:
-                    img_msg = self.bridge.cv2_to_imgmsg(img, encoding='bgr8')
-                    self.img_pub.publish(img_msg)
-                except CvBridgeError as e:
-                    print(e)
+                self.cmd_vel_pub.publish(command)
+            
+        
 
     def get_line_pos(self, img):
         H, W, _ = img.shape
-        img = img[(H-IMG_H)//2:(H+IMG_H)//2, (W-IMG_W)//2:(W+IMG_W)//2] #crop
+        #crops the image (calculate where in the frame the line will be)
 
+        img = img[200:250, :]
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         mask = cv2.inRange(img, RGB_LOW, RGB_HIGH)
         line_img = cv2.bitwise_and(gray, mask)
+        
+        imageOut = self.bridge.cv2_to_imgmsg(line_img)
+        self.img_line.publish(imageOut)
+        
 
         contours, _ = cv2.findContours(np.uint8(line_img), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
+        
+        
         if len(contours) > 0:
             line = max(contours, key=cv2.contourArea)
-            if cv2.contourArea(line) > 3000:
+            print(cv2.contourArea(line))
+            if cv2.contourArea(line) > 30:
                 moments = cv2.moments(line)
                 cx = int(moments['m10']/moments['m00'])
-                cy = int(moments['m01']/moments['m00'])
-
-                # if DISPLAY_IMG:
-                #     cv2.drawContours(img, contours, -1, (180,255,180), 3)
-                #     cv2.drawContours(img, [line], -1, (0,255,0), 3)
-                #     cv2.circle(img, (cx,cy))
+                #cy = int(moments['m01']/moments['m00'])
+                return cx
+        else: 
+            return 0  
+        
+                
+                
+def main():
+    node = WebcamControl()
 
 if __name__ == "__main__":
-    try:
-        node = WebcamControl()
-    except:
-        pass
+    main()
+        
